e44ebc46fc921806e17938907ba32be66ee0c749
Abhishek Singh <singh.abhishek21@gmail.com>
2016-10-29 14:13:17 -0700

    Master_module: skeleton code for CAN rx/tx with each module

--- Master_module/L5_Application/periodic_scheduler/period_callbacks.cpp
+++ Master_module/L5_Application/periodic_scheduler/period_callbacks.cpp
@@ -31,7 +31,43 @@
 #include <stdint.h>
 #include "io.hpp"
 #include "periodic_callback.h"
+#include "can.h"
+#include "_can_dbc/generated_can.h"
 
+can_msg_t rx_msg = {0};
+can_msg_t tx_msg = {0};
+bool stop_all_enable;
+bool start_clicked;
+
+const uint32_t                             COM_BRIDGE_HEARTBEAT__MIA_MS = 3000;
+const COM_BRIDGE_HEARTBEAT_t               COM_BRIDGE_HEARTBEAT__MIA_MSG = {0};
+const uint32_t                             GPS_HEARTBEAT__MIA_MS = 3000;
+const GPS_HEARTBEAT_t                      GPS_HEARTBEAT__MIA_MSG = {0};
+const uint32_t                             MOTOR_HEARTBEAT__MIA_MS = 3000;
+const MOTOR_HEARTBEAT_t                    MOTOR_HEARTBEAT__MIA_MSG = {0};
+const uint32_t                             SENSOR_HEARTBEAT__MIA_MS = 3000;
+const SENSOR_HEARTBEAT_t                   SENSOR_HEARTBEAT__MIA_MSG = {0};
+const uint32_t                             COM_BRIDGE_CLICKED_START__MIA_MS = 3000;
+const COM_BRIDGE_CLICKED_START_t           COM_BRIDGE_CLICKED_START__MIA_MSG = {0};
+const uint32_t                             COM_BRIDGE_STOPALL__MIA_MS = 3000;
+const COM_BRIDGE_STOPALL_t                 COM_BRIDGE_STOPALL__MIA_MSG = {0};
+const uint32_t                             SENSOR_SONARS__MIA_MS = 3000;
+const SENSOR_SONARS_t                      SENSOR_SONARS__MIA_MSG = {8,8,8,8};
+const uint32_t                             GPS_MASTER_DATA__MIA_MS = {3000};
+const GPS_MASTER_DATA_t                    GPS_MASTER_DATA__MIA_MSG = {0};
+
+
+
+COM_BRIDGE_HEARTBEAT_t com_bridge_heartbeat_status = {0};
+GPS_HEARTBEAT_t gps_heartbeat_status = {0};
+MOTOR_HEARTBEAT_t motor_heartbeat_status = {0};
+SENSOR_HEARTBEAT_t sensor_heartbeat_status = {0};
+COM_BRIDGE_CLICKED_START_t com_bridge_start = {0};
+COM_BRIDGE_STOPALL_t com_bridge_stop = {0};
+SENSOR_SONARS_t sensor_data = {0};
+GPS_MASTER_DATA_t gps_data = {0};
+
+MASTER_DRIVING_CAR_t motor_drive = {STOP, CENTER, LOW};
 
 
 /// This is the stack size used for each of the period tasks (1Hz, 10Hz, 100Hz, and 1000Hz)
@@ -48,6 +84,11 @@ const uint32_t PERIOD_DISPATCHER_TASK_STACK_SIZE_BYTES = (512 * 3);
 /// Called once before the RTOS is started, this is a good place to initialize things once
 bool period_init(void)
 {
+	stop_all_enable = 0;
+	start_clicked = 0;
+	CAN_init(can1, 100, 50, 50, NULL, NULL);
+	CAN_bypass_filter_accept_all_msgs();
+	CAN_reset_bus(can1);
     return true; // Must return true upon success
 }
 
@@ -66,17 +107,78 @@ bool period_reg_tlm(void)
 
 void period_1Hz(uint32_t count)
 {
-    LE.toggle(1);
+	// BUS RESET
+	if(CAN_is_bus_off(can1))
+		CAN_reset_bus(can1);
+
+	// HEARTBEAT STATUS ON LEDS
+	if(com_bridge_heartbeat_status.COM_BRIDGE_HEARTBEAT_UNSIGNED == COM_BRIDGE_HEARTBEAT_HDR.mid)
+		LE.on(1);
+	else
+		LE.off(1);
+
+	if(gps_heartbeat_status.GPS_HEARTBEAT_UNSIGNED == GPS_HEARTBEAT_HDR.mid)
+		LE.on(2);
+	else
+		LE.off(2);
+
+	if(motor_heartbeat_status.MOTOR_HEARTBEAT_UNSIGNED == MOTOR_HEARTBEAT_HDR.mid)
+		LE.on(3);
+	else
+		LE.off(3);
+
+	if(sensor_heartbeat_status.SENSOR_HEARTBEAT_UNSIGNED == SENSOR_HEARTBEAT_HDR.mid)
+		LE.on(4);
+	else
+		LE.off(4);
 }
 
 void period_10Hz(uint32_t count)
 {
-    LE.toggle(2);
+	dbc_msg_hdr_t msg_header;
+	if(CAN_rx(can1, &rx_msg, 0))
+	{
+		msg_header.mid = rx_msg.msg_id;
+		msg_header.dlc = rx_msg.frame_fields.data_len;
+
+		if(msg_header.mid == COM_BRIDGE_HEARTBEAT_HDR.mid)
+			dbc_decode_COM_BRIDGE_HEARTBEAT(&com_bridge_heartbeat_status, rx_msg.data.bytes, &msg_header);
+		if(msg_header.mid == GPS_HEARTBEAT_HDR.mid)
+			dbc_decode_GPS_HEARTBEAT(&gps_heartbeat_status, rx_msg.data.bytes, &msg_header);
+		if(msg_header.mid == MOTOR_HEARTBEAT_HDR.mid)
+			dbc_decode_MOTOR_HEARTBEAT(&motor_heartbeat_status, rx_msg.data.bytes, &msg_header);
+		if(msg_header.mid == SENSOR_HEARTBEAT_HDR.mid)
+			dbc_decode_SENSOR_HEARTBEAT(&sensor_heartbeat_status, rx_msg.data.bytes, &msg_header);
+		if(msg_header.mid == COM_BRIDGE_CLICKED_START_HDR.mid)
+			dbc_decode_COM_BRIDGE_CLICKED_START(&com_bridge_start, rx_msg.data.bytes, &msg_header);
+		if(msg_header.mid == COM_BRIDGE_STOPALL_HDR.mid)
+			dbc_decode_COM_BRIDGE_STOPALL(&com_bridge_stop, rx_msg.data.bytes, &msg_header);
+		if(msg_header.mid == SENSOR_SONARS_HDR.mid)
+			dbc_decode_SENSOR_SONARS(&sensor_data, rx_msg.data.bytes, &msg_header);
+		if(msg_header.mid == GPS_MASTER_DATA_HDR.mid)
+			dbc_decode_GPS_MASTER_DATA(&gps_data, rx_msg.data.bytes, &msg_header);
+
+	}
+	dbc_handle_mia_COM_BRIDGE_HEARTBEAT(&com_bridge_heartbeat_status, 100);
+	dbc_handle_mia_GPS_HEARTBEAT(&gps_heartbeat_status, 100);
+	dbc_handle_mia_MOTOR_HEARTBEAT(&motor_heartbeat_status, 100);
+	dbc_handle_mia_SENSOR_HEARTBEAT(&sensor_heartbeat_status, 100);
+
+	// Incrementing time by 0 so that mia will always be in disable state (For time being. Need to give more thought on this)
+	dbc_handle_mia_COM_BRIDGE_CLICKED_START(&com_bridge_start, 0);
+	dbc_handle_mia_COM_BRIDGE_STOPALL(&com_bridge_stop, 0);
+
+	dbc_handle_mia_SENSOR_SONARS(&sensor_data, 100);
+	dbc_handle_mia_GPS_MASTER_DATA(&gps_data, 100);
+
+	msg_header = dbc_encode_MASTER_DRIVING_CAR(tx_msg.data.bytes, &motor_drive);
+	tx_msg.msg_id = msg_header.mid;
+	tx_msg.frame_fields.data_len = msg_header.dlc;
+	CAN_tx(can1, &tx_msg, 0);
 }
 
 void period_100Hz(uint32_t count)
 {
-    LE.toggle(3);
 }
 
 // 1Khz (1ms) is only run if Periodic Dispatcher was configured to run it at main():
